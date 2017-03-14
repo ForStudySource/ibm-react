0b10993c536792fe41fff0cfebf0a289a1650e8b
NAG <nagabhushanamn@gmail.com>
2017-03-14 18:06:08 +0530

    js concepts

--- JS-learn/quiz/quiz-3.js
+++ JS-learn/quiz/quiz-3.js
@@ -2,7 +2,6 @@
 
 
 function sessionStart() {
-    
     function Trainer(name) {
         this.name = name;
         this.doTeach = function () {
@@ -14,22 +13,15 @@ function sessionStart() {
             return doLearn;
         }
     }
-
     function Employee(name) {
         this.name = name;
     }
-
-
     var tnr1 = new Trainer('Nag'); // constructor-invocation
-
     var e1 = new Employee('A');
     var e2 = new Employee('B');
-
     var learnFunc = tnr1.doTeach(); // method-invocation
-    
     learnFunc.call(e1); //call invocation
     learnFunc.call(e2);
-
 }
 
 
