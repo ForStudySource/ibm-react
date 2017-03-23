7295ae0f149204132d6d690281dbda1d6c06f217
NAG <nagabhushanamn@gmail.com>
2017-03-23 14:25:28 +0530

    js concepts.

--- week2/ES6-modules/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/pack1/module1.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-var productId = exports.productId = 123;
--- week2/ES6-modules/dist/dist/dist/dist/dist/dist/pack1/module1.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-var productId = exports.productId = 123;
-var productName = exports.productName = "Laptop";
